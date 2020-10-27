8289bd1c13474be20491c435d067e4057aa023eb
Rowley <notgfx@gmail.com>
2020-10-27 14:23:45 +0900

    3rd case #

--- caller.py
+++ caller.py
@@ -3,6 +3,6 @@ import main
 
 # Кейс 3
 
-main.create_table_if_not_exists()
-main.add_customer('999')
-main.show_all_rows()
+# main.create_table_if_not_exists()
+# main.add_customer('999')
+# main.show_all_rows()
